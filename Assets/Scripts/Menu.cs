89fc1de3b690d680d6228e2ee65e05f4b222db65
CankivancYigit <CankivancYigit@github.com>
2021-01-15 03:37:36 +0300

    Initial Commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+        FindObjectOfType<GameSession>().ResetGameSession();
+    }
+
+     public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
